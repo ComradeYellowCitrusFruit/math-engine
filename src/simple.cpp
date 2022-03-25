a1edc42c1002a7403089de21c0485328905ffa1a
Teresa Fuckyouimnotputtingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-25 13:44:26 -0400

    Add new constants and new file for simple math
    Ditto

---
+++ src/simple.cpp
@@ -0,0 +1,62 @@
+#include "simple.h"
+#include <cmath>
+#include "constants.h"
+#include "converters.h"
+
+int factorial(int x) {
+    if(x == 0 || x == 1) {
+        return(1);
+    }
+    int r=x;
+    for(int i=1; i < x; i++) {
+        r *= (x-i);
+    }
+    return(r);
+}
+
+double factorialWS(double x, double step) {
+    if(x == 0) {
+        return(1);
+    }
+    double r=x;
+    for(int i=1; i < x; i + step) {
+        r *= (x-i);
+    }
+    return(r);
+}
+
+// Does the actual math for the golden ratio, if you need some extra precision
+long double preciseGoldenRatio() {
+    double x = sin(angleConverter(54, 'd'));
+    long double y = 2*(long double)x;
+    return(y);
+}
+
+// Does the actual math for pi with 52 digits of accuracy, which is over kill but if you want 52 digits of accuracy then you can have it.
+long double precisePI() {
+    double x = 236674 + 30303*sqrt(61);
+    x = pow(x, 3);
+    x *= pow(5280, 3);
+    x += 744;
+    x = log(x);
+    double y = sqrt(427);
+    long double X = (long double)x;
+    long double Y = (long double)y;
+    long double z = X/Y;
+    return(z)
+}
+
+// Does the actual math for pi with 161 digits of accuracy, by all means beyond over kill but you do you I guess
+long double overkillPI() {
+    double a = 23 + 4 * sqrt(34);
+    a *= 1/2;
+    double b = 19 * SQRT2 + 7 * sqrt(17);
+    b *= 1/2;
+    double c = 429 + 304 * SQRT2;
+    double d = 627 + 442 * SQRT2;
+    d *= 1/2;
+    double u = pow(a + sqrt(a*a-1), 2) * pow(b + sqrt(b*b-1), 2) * (c + sqrt(c*c-1)) * (d + sqrt(d*d-1))
+    long double x = (long double)(log(pow(2*u,6) + 24))
+    long double y = (long double)(sqrt(3502))
+    return(x/y)
+}
