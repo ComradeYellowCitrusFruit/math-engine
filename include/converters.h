83eefdc43d36963c6ccb8f6b7225477179b03e0a
Teresa Fuckyouimnotputtingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-25 08:49:07 -0400

    No more converter classes

--- include/converters.h
+++ include/converters.h
@@ -3,19 +3,7 @@
 #include "constants.h"
 #include <cmath>
 
-class angleConverter {
-    public:
-        angle(double x, char RorD);
-        double toRad();
-        double toRad(double d);
-        double toDeg();
-        double toDeg(double r);
-        void setDegrees();
-        double getDegrees();
-        void setRadians();
-        double getRadians();
-    private:
-        double degrees;
-        double radians;
-}
+double angleConverter(double x; char RorD);
+double temperature(double x, char type, char to);
+
 #endif
