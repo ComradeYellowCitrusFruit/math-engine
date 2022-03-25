75f9c600eed554319fc35f947bb18a7c1e20fd78
Teresa Fuckyouimnotputtingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-25 13:57:45 -0400

    q̸͙̲͗́͋̏͐̈́̉̀̓̎̓̆̔̄͝ũ̴͇̓́̌̂̕a̴̢̭̞͎̯͇͈̦͈͆̄̈̿̋̿͋̈́͜t̷͈̫̳̯̟̲̞̪̞̟̎̈́͆̌̅̂́͐͒͛̔̐̚͘e̷̥͙̖̮͕̦͖̖̭̫͎̩̮͖͕̐͊̈́͆̒̚ͅŗ̷̢̧̡̢͎̟̩͎̰̞̮͕̩͎͓̞̔́̊̍̇̓̂̒ņ̶̻͙̭̰̠̦̥̦̜̬̻͈̒̌̑́̎̅̅ͅi̸̱͓̗̱̘͎̯̜̫̻͕̽̈́͂̀́̑ȍ̸̧̞̥͔͓͇̟͉̕͝ň̵̲̪̺̉̔͛̽̄̌͌͠s̷̞̜̩̑̌̊̂̒̌̔̿̋̃̌̎̄̋

---
+++ src/quaternions.cpp
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Enter quaternions.                                                                                   //
+// Since my days as a (wannabe) game dev, I feared quaternions.                                         //
+// They confused me, made my life difficult, made it hell for a young, innocent game dev.               //
+// I thought I left them behind, but here I am, once again face to face with quaternions.               //
+// May God have mercy on my soul.                                                                       //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <cmath>
+#include "simple.h"
+#include "environment.h"
+#include "constants.h"
+#include "matrix.h"
+
+class quaternion {
+
+};
